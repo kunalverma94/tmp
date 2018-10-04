0be8387ba8cb273fb950602215b560bafaba6822
kunalverma94 <34855574+kunalverma94@users.noreply.github.com>
2018-10-04 20:10:56 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+const setting =
+{
+    nav: false,
+}
+function Toggle() {
+    var toggler = document.getElementById("toggler");
+    document.getElementById("sidenav").classList.toggle("nav_open");
+    document.getElementById("main").classList.toggle("main_open");
+    toggler.classList.toggle("togggler_left");
+    toggler.innerHTML == "☰" ? toggler.innerHTML = '&times;' : toggler.innerHTML = "☰"
+
+}
+window.onload = function () {
+}
+
